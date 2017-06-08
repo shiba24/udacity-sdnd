ef89e2b22d66f2934d141716b166f3bb8f390abd
Shintaro Shiba <shiba.shintaro@gmail.com>
2017-06-08 19:54:55 +0900

    add p3

---
+++ CarND-Behavioral-Cloning-P3-master/clone.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+import os
+import cv2
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+import argparse
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Dropout, Flatten, Lambda, Cropping2D
+from keras.layers import Conv2D, MaxPooling2D
+
+DATA_DIR = '../data/behavioral-cloning-data/data'
+
+
+def load_data():
+    image_dir = os.path.join(DATA_DIR, 'IMG')
+    log_csv = os.path.join(DATA_DIR, 'driving_log.csv')
+    log_df = pd.read_csv(log_csv, names=['center', 'left', 'right', 'steering', 'throttle', 'break', 'speed'])
+
+    X = np.array([cv2.imread(x) for x in log_df['center']], dtype=np.uint8)
+    y = log_df['steering'].as_matrix()
+    return X, y
+# X = np.transpose(X, (0, 3, 1, 2))
+
+
+def augment(X, y):
+    flipped_X = np.array([cv2.flip(i, 1) for i in X])
+    flipped_y = -1.0 * y
+    aug_X = np.concatenate([X, flipped_X])
+    aug_y = np.concatenate([y, flipped_y])
+    return aug_X, aug_y
+
+def training(X, y, modelname='model.h5', gpu=-1):
+    if gpu < 0:
+        print("Using CPU and LeNet...")
+        model = Sequential()
+        model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=X.shape[1:]))
+        model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid')) #, dim_ordering='tf'))
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1))
+    else:
+        print("Using GPU and LeNet...")
+        model = Sequential()
+        model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=X.shape[1:]))
+        model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid')) #, dim_ordering='tf'))
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(X, y, validation_split=0.2, shuffle=True, nb_epoch=4)
+    model.save(modelname)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        description='GPU flag')
+    args = parser.parse_args()
+    X, y = load_data()
+    X, y = augment(X, y)
+    training(X, y, modelname='model.h5', gpu=args.gpu)
+
+
+
+
+"""
+history_object = model.fit_generator(train_generator, samples_per_epoch =
+    len(train_samples), validation_data = 
+    validation_generator,
+    nb_val_samples = len(validation_samples), 
+    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+"""
